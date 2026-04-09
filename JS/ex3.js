b8a3ceb1a4b56f4a527f8a9106b6b815e393b0c9
Dan Brendza <dan.brendza01@student.csulb.edu>
2026-04-09 13:36:36 -0700

    Ex3

---
+++ JS/ex3.js
@@ -0,0 +1,25 @@
+// Build the data object to send
+const travelData = {
+  name: "Dan",
+  countries: [
+    { name: "Japan", year: 2023 },
+    { name: "Canada", year: 2021 },
+    { name: "France", year: 2022 }
+  ]
+};
+
+// Send the data to the server
+fetch("https://thejsway-server.herokuapp.com/api/countries", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(travelData)
+})
+.then(response => response.json())
+.then(result => {
+  console.log("Server confirmation:", result.message);
+})
+.catch(error => {
+  console.error("Error sending travel data:", error);
+});
